993e2d67ce8fdb3d6c7d7e6155f85f2604fcdf14
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2026-01-18 10:31:12 +0530

    keep only frontend folder

---
+++ frontend/src/app/service/employee-service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../model/employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private baseUrl = 'http://localhost:8081/api/v1/employees';
+
+  constructor(private http: HttpClient) { }
+
+  // Create (Post a List like your Spring Controller)
+  saveEmployees(employees: Employee[]): Observable<Employee[]> {
+    return this.http.post<Employee[]>(this.baseUrl, employees);
+  }
+
+  // Read All
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.baseUrl);
+  }
+
+  // Update
+  updateEmployee(id: number, employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.baseUrl}/${id}`, employee);
+  }
+
+  // Delete
+  deleteEmployee(id: number): Observable<string> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+}
