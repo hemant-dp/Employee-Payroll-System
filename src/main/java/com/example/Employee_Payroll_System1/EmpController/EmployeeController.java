43d9ef66c535bff983514302db0e59a8ccd37d0c
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2026-01-20 19:10:40 +0530

    CORS update url

--- src/main/java/com/example/Employee_Payroll_System1/EmpController/EmployeeController.java
+++ src/main/java/com/example/Employee_Payroll_System1/EmpController/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://127.0.0.1:56991")
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
